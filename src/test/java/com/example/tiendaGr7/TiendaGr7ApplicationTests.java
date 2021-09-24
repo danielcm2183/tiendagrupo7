3f4862af9463651b94ec03a8e04f8240fed6bde9
Daniel Calderon <danielcm2183@hotmail.com>
2021-09-24 10:51:06 -0500

    prueba

---
+++ src/test/java/com/example/tiendaGr7/TiendaGr7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendaGr7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGr7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
